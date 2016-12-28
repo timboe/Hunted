fda01386613541a10adfa92f7df0c815424d1b16
Tim Martin <timboe@gmail.com>
2016-12-28 14:21:54 +0000

    Add fresh libgdx

---
+++ desktop/src/timboe/hunted/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package timboe.hunted.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import timboe.hunted.HuntedGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HuntedGame(), config);
+	}
+}
